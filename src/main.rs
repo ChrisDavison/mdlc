d833675ea220fe52714003b5639d019264a99e29
Chris Davison <c.jr.davison@gmail.com>
2020-06-22 09:57:20 +0100

    Remove structopt

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,74 @@
-#![allow(unused_variables, dead_code)]
-use std::path::PathBuf;
+use std::env;
 
 use glob::glob;
-use structopt::StructOpt;
 
 #[macro_use]
 extern crate lazy_static;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[derive(StructOpt, Debug)]
-struct Opts {
-    /// Files to check links in
-    files: Vec<std::path::PathBuf>,
+struct Args {
+    files: Vec<String>,
+    linktype: Option<links::LinkType>,
+}
 
-    /// Only check local links
-    #[structopt(short, long)]
-    local: bool,
-
-    /// Only check web links
-    #[structopt(short, long)]
-    web: bool,
+fn parse_args() -> Result<Args> {
+    let mut files = Vec::new();
+    let mut linktype: Option<links::LinkType> = None;
+    for a in env::args().skip(1) {
+        if !a.starts_with("-") {
+            files.push(a);
+        } else {
+            if a == "-l" || a == "--local" {
+                linktype = Some(links::LinkType::Local);
+            } else if a == "-w" || a == "--web" {
+                linktype = Some(links::LinkType::Web);
+            } else {
+                println!("Unrecognised Flag {}", a);
+            }
+        }
+    }
+    if files.is_empty() {
+        files = get_md_files_in_curdir()?
+    }
+    Ok(Args{files, linktype})
 }
 
 fn main() -> Result<()> {
-    let args = Opts::from_args();
-    let files = if args.files.is_empty() {
-        get_md_files_in_curdir()?
-    } else {
-        args.files
-    };
-    for filename in files {
-        let lt = if args.local {
-            Some(links::LinkType::Local)
-        } else if args.web {
-            Some(links::LinkType::Web)
-        } else {
-            None
-        };
-        let links: Vec<links::Link> = links::from_file(&filename, lt)
-            .iter()
-            .filter(|l| !l.is_alive())
-            .map(|x| x.to_owned())
-            .collect();
+    let args = parse_args()?;
+    let linkfilter = args.linktype;
 
-        let fn_str = filename.to_string_lossy().to_string();
+    for filename in args.files {
+        let mut links: Vec<links::Link> = Vec::new();
+        for link in links::from_file(&filename) {
+            if !(linkfilter == None || link.linktype == linkfilter.unwrap()) {
+                continue
+            }
+            if link.is_alive() {
+                links.push(link);
+            }
+        }
 
         for link in links {
-            println!("{}:{:?}:{}", fn_str, link.linktype, link.text);
+            println!("{}:{:?}:{}", filename, link.linktype, link.text);
         }
     }
     Ok(())
 }
 
 /// Glob for markdown files under the current working directory
-fn get_md_files_in_curdir() -> Result<Vec<PathBuf>> {
+fn get_md_files_in_curdir() -> Result<Vec<String>> {
     Ok(glob("**/*.md")?
         .filter(|x| x.is_ok())
-        .map(|x| x.expect("Already tested each glob is ok"))
+        .map(|x| x.expect("Already tested each glob is ok").to_string_lossy().to_string())
         .collect())
 }
 
 mod links {
     use regex::Regex;
     use std::collections::HashSet;
-    use std::path::PathBuf;
 
-    #[derive(Eq, PartialEq, Debug, Clone)]
+    #[derive(Eq, PartialEq, Debug, Clone, Copy)]
     pub enum LinkType {
         Web,
         Local,
@@ -79,7 +81,7 @@ mod links {
         pub linktype: LinkType,
     }
 
-    pub fn from_file(filename: &PathBuf, linktype: Option<LinkType>) -> Vec<Link> {
+    pub fn from_file(filename: &str) -> Vec<Link> {
         lazy_static! {
             static ref RE_WEB: Regex = Regex::new(
                 r#"(https+://)*[-a-zA-Z0-9@:%._/\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9@:%_\+.~#?&//=]*"#
@@ -89,43 +91,38 @@ mod links {
         }
         let contents = std::fs::read_to_string(filename).unwrap();
         let mut links = Vec::new();
-        let fn_str = filename.to_string_lossy().to_string();
         let mut seen = HashSet::new();
 
-        if linktype == None || linktype == Some(LinkType::Local) {
-            for cap in RE_LOCAL.captures_iter(&contents) {
-                let linktext: String = if cap[0].starts_with("(") {
-                    cap[0][1..].into()
-                } else {
-                    cap[0].into()
-                };
-                if seen.contains(&linktext) {
-                    continue;
-                } else {
-                    seen.insert(linktext.clone());
-                }
-                links.push(Link {
-                    text: linktext.clone(),
-                    linktype: LinkType::Local,
-                    source: fn_str.clone(),
-                });
+        for cap in RE_LOCAL.captures_iter(&contents) {
+            let linktext: String = if cap[0].starts_with("(") {
+                cap[0][1..].into()
+            } else {
+                cap[0].into()
+            };
+            if seen.contains(&linktext) {
+                continue;
+            } else {
+                seen.insert(linktext.clone());
             }
+            links.push(Link {
+                text: linktext.clone(),
+                linktype: LinkType::Local,
+                source: filename.to_string(),
+            });
         }
 
-        if linktype == None || linktype == Some(LinkType::Web) {
-            for cap in RE_WEB.captures_iter(&contents) {
-                let linktext: String = cap[0].into();
-                if seen.contains(&linktext) {
-                    continue;
-                } else {
-                    seen.insert(linktext.clone());
-                }
-                links.push(Link {
-                    text: linktext.clone(),
-                    linktype: LinkType::Web,
-                    source: fn_str.clone(),
-                });
+        for cap in RE_WEB.captures_iter(&contents) {
+            let linktext: String = cap[0].into();
+            if seen.contains(&linktext) {
+                continue;
+            } else {
+                seen.insert(linktext.clone());
             }
+            links.push(Link {
+                text: linktext.clone(),
+                linktype: LinkType::Web,
+                source: filename.to_string(),
+            });
         }
         links
     }
@@ -139,19 +136,11 @@ mod links {
             }
         }
 
-        pub fn is_local(&self) -> bool {
-            self.linktype == LinkType::Local
-        }
-
-        pub fn is_web(&self) -> bool {
-            self.linktype == LinkType::Web
-        }
-
         fn is_valid_local_link(&self) -> bool {
             let source = std::path::PathBuf::from(&self.source);
             let parent = source.parent().unwrap();
             let p = parent.join(&self.text).canonicalize();
-            if let Err(e) = p {
+            if let Err(_e) = p {
                 false
             } else {
                 p.unwrap().exists()
@@ -161,7 +150,7 @@ mod links {
         fn is_valid_web_link(&self) -> bool {
             match reqwest::blocking::get(&self.text) {
                 Ok(resp) => resp.status() == 200,
-                Err(e) => false,
+                Err(_e) => false,
             }
         }
     }
